feedb78f27107cd2a8e8073ab74a5f2a661e788e
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-29 10:09:06 +0330

    add services

---
+++ Automation.DataModelLayer/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Automation.DataModelLayer.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automation.DataModelLayer
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUsers,ApplicationRoles,string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(option)
+        {
+
+        }
+    }
+}
